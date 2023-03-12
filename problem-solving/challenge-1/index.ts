0fbe9227d2eb4ed4bdcb8357ebb1b5f5e33859b2
Adex0494 <adex0494@gmail.com>
2023-03-12 11:50:55 -0400

    challenge 1 completed

--- problem-solving/challenge-1/index.ts
+++ problem-solving/challenge-1/index.ts
@@ -1,1 +1,26 @@
-export const numbersFractionCalculator = (numbers: number[]) => {};
+interface Challenge1Result {
+  positives: string,
+  negative: string,
+  zeros: string
+}
+
+export const numbersFractionCalculator  = (numbers: number[]): Challenge1Result  =>  {
+  let positives = 0;
+  let negatives = 0;
+  let zeros = 0;
+
+  const numbersLength = numbers.length
+
+  for (let i = 0; i<numbersLength; i++){
+    const number = numbers[i]
+    if (number > 0) {positives++}
+    else if(number){negatives++}
+    else zeros++
+  }
+
+  return {
+    positives: (positives/numbersLength).toFixed(6),
+    negative: (negatives/numbersLength).toFixed(6),
+    zeros: (zeros/numbersLength).toFixed(6)
+  }
+};
